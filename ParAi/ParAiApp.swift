4dcd43ba255d7ac2fa96578ff74c48eb708e86e5
Hasan Kilinc <hasankilincce@gmail.com>
2025-05-24 21:10:54 +0300

    Initial Commit

---
+++ ParAi/ParAiApp.swift
@@ -0,0 +1,32 @@
+//
+//  ParAiApp.swift
+//  ParAi
+//
+//  Created by Hasan Kılınç on 24.05.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ParAiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ParAiTests/ParAiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParAiTests.swift
+//  ParAiTests
+//
+//  Created by Hasan Kılınç on 24.05.2025.
+//
+
+import Testing
+@testable import ParAi
+
+struct ParAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
